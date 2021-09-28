4a4cdcf71e9ed4f089fe11f8214520e220e9a1a1
Tasha Davis <tdavis0831@yahoo.com>
2021-09-28 02:13:16 +0000

    changes, completed with notes

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,26 +6,23 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
-from flask import session
-import melons
 
+import melons
 
 app = Flask(__name__)
 
 # A secret key is needed to use Flask sessioning features
+
 app.secret_key = 'this-should-be-something-unguessable'
 
 # Normally, if you refer to an undefined variable in a Jinja template,
 # Jinja silently ignores this. This makes debugging difficult, so we'll
 # set an attribute of the Jinja environment that says to make this an
 # error.
-app.jinja_env.undefined = jinja2.StrictUndefined
 
-# This configuration option makes the Flask interactive debugger
-# more useful (you should remove this line in production though)
-app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+app.jinja_env.undefined = jinja2.StrictUndefined
 
 
 @app.route("/")
@@ -39,23 +36,25 @@ def index():
 def list_melons():
     """Return page showing all the melons ubermelon has to offer"""
 
-    melon_list = melons.get_all()
+    melon_list = melons.get_all() #instances of all the melon classes
     return render_template("all_melons.html",
                            melon_list=melon_list)
 
 
-@app.route("/melon/<melon_id>")
-def show_melon(melon_id):
+@app.route("/melon/<melon_id>") #a variable route
+#if you go to /melon/alib, show_melon will be called with alib as the parameter
+def show_melon(melon_id):#the melon id comes from the url
     """Return page showing the details of a given melon.
 
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(melon_id)
+    melon = melons.get_by_id(melon_id) #can pass in the melon id from the paramateres because it comes
+    #from the parameters
     print(melon)
-    print(melon_id)
     return render_template("melon_details.html",
                            display_melon=melon)
+
 
 @app.route("/cart")
 def show_shopping_cart():
@@ -70,6 +69,8 @@ def show_shopping_cart():
 
     # Get the cart dictionary out of the session (or an empty one if none
     # exists yet)
+
+    #session['cart' {melon_id: num of times it was added}]
     cart = session.get("cart", {})
 
     # Loop over the cart dictionary
@@ -77,14 +78,19 @@ def show_shopping_cart():
         # Retrieve the Melon object corresponding to this id
         melon = melons.get_by_id(melon_id)
 
+        # Calculate the total cost for this type of melon and add it to the
+        # overall total for the order
         total_cost = quantity * melon.price
         order_total += total_cost
+
+        # Add the quantity and total cost as attributes on the Melon object
         melon.quantity = quantity
         melon.total_cost = total_cost
 
-    
+        # Add the Melon object to our list
         cart_melons.append(melon)
 
+    # Pass the list of Melon objects and the order total to our cart template
 
     return render_template("cart.html",
                            cart=cart_melons,
@@ -99,27 +105,36 @@ def add_to_cart(melon_id):
     page and display a confirmation message: 'Melon successfully added to
     cart'."""
 
-    # TODO: Finish shopping cart functionality
-
-    # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
-    # - check if the desired melon id is the cart, and if not, put it in
-    # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
-
+    # Check if we have a cart in the session and if not, add one
+    # Also, bind the cart to the name 'cart' for easy reference below
     if 'cart' in session:
         cart = session['cart']
     else:
-        cart = session['cart']={}
+        cart = session['cart'] = {}
 
-    cart[melon_id] = cart.get(melon_id, 0)+1
+    # We could also do this with setdefault:
+    # cart = session.setdefault("cart", {})
 
-    flash("melon added to cart")
+    # Add melon to cart - either increment the count (if melon already in cart)
+    # or add to cart with a count of 1
+    cart[melon_id] = cart.get(melon_id, 0) + 1
 
-    return redirect('/cart')
+
+    # if melon_id not in cart:
+    #     cart[melon_id] = 0        these lines are also doing what the line about is doing
+    
+    # cart[melon_id] +=1
+
+
+    # Print cart to the terminal for testing purposes
+    # print("cart:")
+    # print(cart)
+
+    # Show user success message on next page load
+    flash("Melon successfully added to cart.")
+
+    # Redirect to shopping cart page
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
@@ -166,4 +181,4 @@ def checkout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host='0.0.0.0')
