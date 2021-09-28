64340152c2d75610783e5a8e5b9c417d2acd30b6
Tasha Davis <tdavis0831@yahoo.com>
2021-09-28 00:18:07 +0000

    lab completed alone

---
+++ shoppingsite.py
@@ -0,0 +1,169 @@
+"""Ubermelon shopping application Flask server.
+
+Provides web interface for browsing melons, seeing detail about a melon, and
+put melons in a shopping cart.
+
+Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
+"""
+
+from flask import Flask, render_template, redirect, flash
+import jinja2
+from flask import session
+import melons
+
+
+app = Flask(__name__)
+
+# A secret key is needed to use Flask sessioning features
+app.secret_key = 'this-should-be-something-unguessable'
+
+# Normally, if you refer to an undefined variable in a Jinja template,
+# Jinja silently ignores this. This makes debugging difficult, so we'll
+# set an attribute of the Jinja environment that says to make this an
+# error.
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+# This configuration option makes the Flask interactive debugger
+# more useful (you should remove this line in production though)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
+
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route("/melons")
+def list_melons():
+    """Return page showing all the melons ubermelon has to offer"""
+
+    melon_list = melons.get_all()
+    return render_template("all_melons.html",
+                           melon_list=melon_list)
+
+
+@app.route("/melon/<melon_id>")
+def show_melon(melon_id):
+    """Return page showing the details of a given melon.
+
+    Show all info about a melon. Also, provide a button to buy that melon.
+    """
+
+    melon = melons.get_by_id(melon_id)
+    print(melon)
+    print(melon_id)
+    return render_template("melon_details.html",
+                           display_melon=melon)
+
+@app.route("/cart")
+def show_shopping_cart():
+    """Display content of shopping cart."""
+
+    # Keep track of the total cost of the order
+    order_total = 0
+
+    # Create a list to hold Melon objects corresponding to the melon_id's in
+    # the cart
+    cart_melons = []
+
+    # Get the cart dictionary out of the session (or an empty one if none
+    # exists yet)
+    cart = session.get("cart", {})
+
+    # Loop over the cart dictionary
+    for melon_id, quantity in cart.items():
+        # Retrieve the Melon object corresponding to this id
+        melon = melons.get_by_id(melon_id)
+
+        total_cost = quantity * melon.price
+        order_total += total_cost
+        melon.quantity = quantity
+        melon.total_cost = total_cost
+
+    
+        cart_melons.append(melon)
+
+
+    return render_template("cart.html",
+                           cart=cart_melons,
+                           order_total=order_total)
+
+
+@app.route("/add_to_cart/<melon_id>")
+def add_to_cart(melon_id):
+    """Add a melon to cart and redirect to shopping cart page.
+
+    When a melon is added to the cart, redirect browser to the shopping cart
+    page and display a confirmation message: 'Melon successfully added to
+    cart'."""
+
+    # TODO: Finish shopping cart functionality
+
+    # The logic here should be something like:
+    #
+    # - check if a "cart" exists in the session, and create one (an empty
+    #   dictionary keyed to the string "cart") if not
+    # - check if the desired melon id is the cart, and if not, put it in
+    # - increment the count for that melon id by 1
+    # - flash a success message
+    # - redirect the user to the cart page
+
+    if 'cart' in session:
+        cart = session['cart']
+    else:
+        cart = session['cart']={}
+
+    cart[melon_id] = cart.get(melon_id, 0)+1
+
+    flash("melon added to cart")
+
+    return redirect('/cart')
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+
+    # TODO: Need to implement this!
+
+    # The logic here should be something like:
+    #
+    # - get user-provided name and password from request.form
+    # - use customers.get_by_email() to retrieve corresponding Customer
+    #   object (if any)
+    # - if a Customer with that email was found, check the provided password
+    #   against the stored one
+    # - if they match, store the user's email in the session, flash a success
+    #   message and redirect the user to the "/melons" route
+    # - if they don't, flash a failure message and redirect back to "/login"
+    # - do the same if a Customer with that email doesn't exist
+
+    return "Oops! This needs to be implemented"
+
+
+@app.route("/checkout")
+def checkout():
+    """Checkout customer, process payment, and ship melons."""
+
+    # For now, we'll just provide a warning. Completing this is beyond the
+    # scope of this exercise.
+
+    flash("Sorry! Checkout will be implemented in a future version.")
+    return redirect("/melons")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
